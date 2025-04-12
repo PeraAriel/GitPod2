6f2728f4dde5f016396d0daf3109d11890837d07
Pera Ariel <arielguia@icloud.com>
2025-04-12 09:28:45 +0000

    Punto 2, Ho risolto tutti i problemi

--- angular-data/src/app/app.component.ts
+++ angular-data/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { UserItemComponent } from './user-item/user-item.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [UserItemComponent, HelloWorldComponent] // Importa i componenti
+  imports: [UserItemComponent, HelloWorldComponent, UserListComponent] // Importa i componenti
 })
 export class AppComponent {
   title = 'angular-data';
--- angular-data/src/app/user-list/user-list.component.ts
+++ angular-data/src/app/user-list/user-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { UserItemComponent } from '../user-item/user-item.component';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
+  imports: [UserItemComponent]
 })
 export class UserListComponent implements OnInit {
   names: string[];
